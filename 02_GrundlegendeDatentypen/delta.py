e126aa54b90d92b6c282a59bb46306b42ac11d49
Fatih Deniz <deniz.fatih@hotmail.de>
2026-06-19 10:05:47 +0200

    Delta Berechnung

---
+++ 02_GrundlegendeDatentypen/delta.py
@@ -0,0 +1,10 @@
+a=input(int("Geben Sie die erste Zahl ein :"))
+b=input(int("Geben Sie die zweite Zahl ein :"))
+c=input(int("Geben Sie die dritte Zahl ein :"))
+
+delta = b**2-4a*c
+
+if delta>0:
+    print("True")
+else:
+    print("False")

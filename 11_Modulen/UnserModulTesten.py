2d49cc912e87b7d96282bc0f9bf7ed65a7cc1d6a
Fatih Deniz <deniz.fatih@hotmail.de>
2026-06-28 02:03:55 +0200

    Modulen

---
+++ 11_Modulen/UnserModulTesten.py
@@ -0,0 +1,9 @@
+import UnserModul as um
+
+student1 = um.Student("Fatih", "Deniz", "Informatik")
+student1.zeige_infos()
+
+print()  # Leerzeile für bessere Lesbarkeit
+
+student2 = um.Student("Max", "Mustermann", "Informatik")
+student2.zeige_infos()

094b8103821b84471efe6914ae29ce84bf691205
Fatih Deniz <deniz.fatih@hotmail.de>
2026-07-10 23:03:01 +0200

    Fehlerbehandlung

---
+++ 10_Fehlerbehandlung/verdoppeln.py
@@ -0,0 +1,8 @@
+#----------------------------------------------------
+# Dateiname:  verdoppeln.py 
+# Modul mit Funktion verdoppeln().
+#----------------------------------------------------
+
+def verdopple(x):
+  assert type(x) in [int, float]
+  return 2 * x

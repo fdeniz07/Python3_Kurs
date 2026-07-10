d0ae7ad18449d33e954cf5e4bca3833e373e3fe6
Fatih Deniz <deniz.fatih@hotmail.de>
2026-07-10 23:03:05 +0200

    Fehlerbehandlung

---
+++ 10_Fehlerbehandlung/zufallsliste.py
@@ -0,0 +1,22 @@
+#----------------------------------------------------
+# Dateiname:  qsort.py 
+# Modul mit Funktion quicksort(), die eine Liste sortiert.
+#
+# Einstieg Python
+# Kap. 12
+# Michael Weigend 21.12.2020
+#----------------------------------------------------
+
+from random import randint
+s = [randint(0, 1000000) for i in range(1000)]
+print(s)
+
+
+
+
+
+
+
+
+
+                    
